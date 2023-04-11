0d6971f09ec39538a9476d215900f7816d1f7d89
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-11 17:09:34 -0300

    Lógica concluída

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 function exibiAnosCopa() {
 	var campoAnoInicial = document.getElementById("ano-inicial");
-
+	var campoAnoFinal = document.getElementById("ano-final");
 	
+	var anoInicial = campoAnoInicial.value;
+	var anoFinal = campoAnoFinal.value;
+
+	anoInicial = parseInt(anoInicial);
+	anoFinal = parseInt(anoFinal);
+
+	var ano = anoInicial;
+
+	while (ano <= anoFinal) {
+		document.write(ano);
+		document.write("<br>");
+		ano = ano + 4;
+	}
 }
 
 var botao = document.querySelector("button");
