ea662ad5c38bd47605d40805004b73d234e675ed
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-11 16:18:36 -0300

    Armazenando elementos HTML

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function exibiAnosCopa() {
+	var campoAnoInicial = document.getElementById("ano-inicial");
+
+	
+}
+
+var botao = document.querySelector("button");
+botao.addEventListener("click", exibiAnosCopa);
