3a86196ed9de96c8dc942f04ab8c160fb811bb60
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-11 17:28:38 -0300

    Verificações dos anos adicionados

--- js/index.js
+++ js/index.js
@@ -1,19 +1,32 @@
 function exibiAnosCopa() {
 	var campoAnoInicial = document.getElementById("ano-inicial");
 	var campoAnoFinal = document.getElementById("ano-final");
-	
+	var mensagem = document.getElementById("mensagem");
+
 	var anoInicial = campoAnoInicial.value;
 	var anoFinal = campoAnoFinal.value;
+
+	console.log(anoInicial);
 
 	anoInicial = parseInt(anoInicial);
 	anoFinal = parseInt(anoFinal);
 
-	var ano = anoInicial;
+	if (isNaN(anoInicial) || isNaN(anoFinal)) {
+		mensagem.textContent = "Digite somente números!";
+	} else if (anoInicial > 9999 || anoFinal > 9999) {
+		mensagem.textContent = "Digite os anos em quatro dígitos!";
+	} else if (anoInicial < 1930) {
+		mensagem.textContent = "A Copa do Mundo começou em 1930!";
+	} else if (anoFinal <= anoInicial){
+		mensagem.textContent = "Ano final tem que ser maior que o ano incial!";
+	} else {
+		var ano = anoInicial;
 
-	while (ano <= anoFinal) {
-		document.write(ano);
-		document.write("<br>");
-		ano = ano + 4;
+		while (ano <= anoFinal) {
+			document.write(ano);
+			document.write("<br>");
+			ano = ano + 4;
+		}
 	}
 }
 
